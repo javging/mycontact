bdc350c479280010bd081e17f1840b2cb38c4850
nanangqq <gingggg@gmail.com>
2021-08-07 14:24:50 +0900

    03-02. 롬복 적용중. @Getter, @Setter 적용

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,8 +1,12 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class Person {
@@ -10,33 +14,97 @@ public class Person {
     @GeneratedValue
     private Long id;
 
+    @Getter
+    @Setter
     private String name;
 
+    @Getter
+    @Setter
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    @Getter
+    @Setter
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Getter
+    @Setter
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    @Getter
+    @Setter
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Getter
+    @Setter
+    private LocalDate birthday;
 
-    public int getAge() {
-        return age;
-    }
+    @Getter
+    @Setter
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+//    public String getHobby() {
+//        return hobby;
+//    }
+//
+//    public void setHobby(String hobby) {
+//        this.hobby = hobby;
+//    }
+//
+//    public String getBloodType() {
+//        return bloodType;
+//    }
+//
+//    public void setBloodType(String bloodType) {
+//        this.bloodType = bloodType;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public LocalDate getBirthday() {
+//        return birthday;
+//    }
+//
+//    public void setBirthday(LocalDate birthday) {
+//        this.birthday = birthday;
+//    }
+//
+//    public String getJob() {
+//        return job;
+//    }
+//
+//    public void setJob(String job) {
+//        this.job = job;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 
     @Override
     public String toString() {
