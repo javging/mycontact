facd21fc9ccaf29d7721b98bd9139fb8ea742686
nanangqq <gingggg@gmail.com>
2021-08-08 15:36:12 +0900

    04-06. PersonRepositoryTest 파일 정리

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -6,12 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -37,93 +32,35 @@ class PersonRepositoryTest {
     @Test
     void crud() {
         Person person = new Person();
-        person.setName("martin");
+        person.setName("john");
         person.setAge(10);
         person.setBloodType("A");
-
-        System.out.println(personRepository);
         personRepository.save(person);
 
-        System.out.println(personRepository.findAll());
-
-        List<Person> people = personRepository.findAll();
-        assertThat(people.size()).isEqualTo(1);
-        assertThat(people.get(0).getName()).isEqualTo("martin");
-        assertThat(people.get(0).getAge()).isEqualTo(10);
-        assertThat(people.get(0).getBloodType()).isEqualTo("A");
+        List<Person> result = personRepository.findByName("john");
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getName()).isEqualTo("john");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+        assertThat(result.get(0).getBloodType()).isEqualTo("A");
     }
 
-    @Test
-    void constructorTest() {
-        // Person person = new Person(1L, "martin", 10, "reading", "A", "분당", LocalDate.of(2021,8,7), "programmer", "010-7141-3096");
-        Person person2 = new Person("martin", 1, "B");
-        // System.out.println(person);
-        person2.setAge(999999999);
-        System.out.println(person2);
-        // System.out.println(person2.isBlock());
-    }
-
-
-    @Test
-    void hashcodeAndEquals() {
-        Person person1 = new Person("martin", 10, "A");
-        Person person2 = new Person("martin", 10, "A");
-
-        System.out.println(person1.equals(person2));
-        System.out.println(person1.hashCode());
-        System.out.println(person2.hashCode());
-        // 해시코드는 다름
-        // 해시코드가 다르면 어떤 문제??
-
-        Map<Person, Integer> map = new HashMap<>();
-        map.put(person1, person1.getAge());
-
-        System.out.println(map);
-        System.out.println(map.get(person2));
-    }
 
     @Test
     void findByBloodType() {
-        personRepository.save(new Person("martin", 10, "A"));
-        personRepository.save(new Person("martin", 10, "B"));
-        personRepository.save(new Person("martin", 10, "C"));
-        // 같은 혈액형 2개 이상이면 오류 발생함,,
-        personRepository.save(new Person("martin", 10, "A"));
-        personRepository.save(new Person("martin", 10, "A"));
+        List<Person> result = personRepository.findByBloodType("A");
 
-//        System.out.println(personRepository.findByBloodType("A")); // 혈액형으로 조회했을 때, 1명만 나오면 Person객체로 받아와도 괜춘
-//        personRepository.findByBloodType("A").forEach(System.out::println);
-        // 여러개일 경우 생각해서 리스트형태로 받아와야 함,,
-        System.out.println(personRepository.findByBloodType("AB")); // 객체로 받아올 때,, 하나도 없으면 null 임 ,, 리스트로 받아올때는 빈 리스트.
-//        personRepository.findByBloodType("AB").forEach(System.out::println);
-        // 없을 경우 빈 리스트,,
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getName()).isEqualTo("martin");
+        assertThat(result.get(1).getName()).isEqualTo("sophia");
     }
 
     @Test
     void findByBirthdayBetween() {
-        givenPerson("martin", 10, "A", LocalDate.of(1988,8,1));
-        givenPerson("martin", 10, "A", LocalDate.of(1988,9,1));
-        givenPerson("martin", 10, "A", LocalDate.of(1988,10,1));
-        givenPerson("martin", 10, "A", LocalDate.of(1988,11,1));
-        givenPerson("martin", 10, "A", LocalDate.of(1989,8,10));
+        List<Person> result = personRepository.findByMonthOfBirthday(8);
 
-//        personRepository.findByBirthdayBetween(
-//                LocalDate.of(1988,8,1),
-//                LocalDate.of(1988, 8,31))
-//                .forEach(System.out::println);
-        // 년도 다를 경우 조회하기 어려움
-        personRepository.findByMonthOfBirthday(8).forEach(System.out::println);
-        personRepository.findByMonthOfBirthdayAndDayOfBirthday(8, 10).forEach(System.out::println);
-        personRepository.anyName(8, 10).forEach(System.out::println);
-        personRepository.nativeQueryTest(8, 10).forEach(System.out::println);
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getName()).isEqualTo("martin");
+        assertThat(result.get(1).getName()).isEqualTo("sophia");
     }
 
-    void givenPerson(String name, int age, String bloodType, LocalDate birthday) {
-        Person person = new Person(name, age, bloodType);
-//        person.setBirthday(birthday);
-//        person.setBirthday(new Birthday(birthday.getYear(), 13, birthday.getDayOfMonth()));
-//        person.setBirthday(new Birthday(birthday.getYear(), birthday.getMonthValue(), birthday.getDayOfMonth()));
-        person.setBirthday(new Birthday(birthday));
-        personRepository.save(person);
-    }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,8 @@
-insert into person(`id`, `name`, `age`, `blood_type`) values(1, 'martin', 10, 'A');
+insert into person (`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (1, 'martin', 10, 'A', 1991, 8, 15);
+insert into person (`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (2, 'david', 10, 'B', 1992, 7, 10);
+insert into person (`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'dennis', 10, 'O', 1993, 10, 5);
+insert into person (`id`, `name`, `age`, `blood_type`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'sophia', 10, 'A', 1994, 8, 31);
+insert into person (`id`, `name`, `age`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'benny', 10, 1995, 12, 23);
+insert into person (`id`, `name`, `age`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`, `hobby`, `phone_number`, `address`)
+values (6, 'tony', 10, 1991, 7, 10, 'officer', 'reading', '010-2222-5555', '서울');
+insert into person (`id`, `name`, `age`) values (7, 'andrew', 10);
